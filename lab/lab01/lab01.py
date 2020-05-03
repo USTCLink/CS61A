61f4d13945dc97b28efcf2e9b376130d372d1c71
Chenhao Fang <cfang45@wisc.edu>
2020-05-02 22:07:33 -0500

    update solution to hw01 and lab01

---
+++ lab/lab01/lab01.py
@@ -0,0 +1,77 @@
+"""Lab 1: Expressions and Control Structures"""
+
+def both_positive(a, b):
+    """Returns True if both a and b are positive.
+
+    >>> both_positive(-1, 1)
+    False
+    >>> both_positive(1, 1)
+    True
+    """
+    return a > 0 and b > 0 # You can replace this line!
+
+def sum_digits(x):
+    """Sum all the digits of x.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    out = 0
+    while x > 0:
+        quotient, reminder = x // 10, x % 10
+        x = quotient
+        out += reminder
+    return out
+    
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    out = 1
+    while k > 0:
+        out = out * n
+        n = n - 1
+        k = k -1
+    return out
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    prev_eight = False
+    while n > 0:
+        last_digit = n%10
+        if last_digit == 8 and prev_eight:
+            return True
+        elif last_digit == 8:
+            prev_eight = True
+        else:
+            prev_eight = False
+        n = n // 10
+    return False
